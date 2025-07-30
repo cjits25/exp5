95731d0b8d9988b8539f2ec3ee6b5f172b11cfdd
Suresh Lagishetti <cjitsjn@gmail.com>
2025-07-30 23:16:06 +0530

    Exp5 project

--- bin/build.bat
+++
@@ -1,9 +0,0 @@
-echo Building Java App...
-mkdir out 2>nul
-javac -d out src\Main.java
-
-echo Running Java App...
-cd out
-java Main
-
-echo Build and run completed.
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) 
 {
-        System.out.println("Hello, World!");
-        int a = 5, b = 7;
+        System.out.println("Hello, India!");
+        int a = 5, b = 70;
         int sum = a + b;
         System.out.println("Sum: " + sum);
     }
